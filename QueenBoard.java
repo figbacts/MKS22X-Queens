48381b85d1cd7d4afbf0509a69eb11fcf62d00e3
Jack Lam <jlam00@stuy.edu>
2019-02-08 11:45:29 -0500

    got remove queen and add queen to work all the time

--- QueenBoard.java
+++ QueenBoard.java
@@ -5,11 +5,30 @@ public class QueenBoard{
   }
   public boolean addQueen(int r, int c){
     board[r][c] = -1;
+    for (int h = c + 1; h < board[r].length; h ++){
+      board[r][h] = board[r][h] + 1;
+      if (r+h-c < board.length){
+        board[r + h - c][h] += 1;
+      }
+      if(r - h  +c > -1 ){
+        board[r - h + c][h] += 1;
+      }
+    }
     return true;
   }
-  //private boolean removeQueen(int r, int c){
-
-  //}
+  private boolean removeQueen(int r, int c){
+    board[r][c] = 0;
+    for (int h = c + 1; h < board[r].length; h ++){
+      board[r][h] = board[r][h] -1;
+      if (r+h-c < board.length){
+        board[r + h - c][h] -= 1;
+      }
+      if(r - h  +c > -1 ){
+        board[r - h + c][h] -= 1;
+      }
+    }
+    return true;
+  }
   public String toString(){
     String ans = "";
     for (int row = 0; row < board.length; row ++){
@@ -21,8 +40,9 @@ public class QueenBoard{
     return ans;
   }
 public static void main(String[] args){
-QueenBoard test = new QueenBoard(10);
-test.addQueen(4,4);
+QueenBoard test = new QueenBoard(4);
+test.addQueen(2,1);
+test.removeQueen(2,1);
 System.out.println(test);
 }
 }

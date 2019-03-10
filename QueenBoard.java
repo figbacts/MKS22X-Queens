dd1055cfc91f445ed1d4c37d1c7e1f94105fd74e
Jack Lam <jlam00@stuy.edu>
2019-03-09 22:28:14 -0500

    use Mr.K driver

--- QueenBoard.java
+++ QueenBoard.java
@@ -94,7 +94,30 @@ public static int helper2(QueenBoard test, int [][] board, int col){
     }
     return ans;
 }
-//public static void main(String[] args){
+public static void runTest(int i){
+  QueenBoard b;
+  int[]tests =   {1,2,3,4,5,8};
+  int[]answers = {1,0,0,2,10,92};
+  if(i >= 0 && i < tests.length ){
+    int size = tests[i];
+    int correct = answers[i];
+    b = new QueenBoard(size);
+    int ans  = b.countSolutions();
+
+    if(correct==ans){
+      System.out.println("PASS board size: "+tests[i]+" "+ans);
+    }else{
+      System.out.println("FAIL board size: "+tests[i]+" "+ans+" vs "+correct);
+    }
+  }
+}
+public static void main(String[] args){
+  //runTest(0);
+  //runTest(1);
+  //runTest(2);
+  //runTest(3);
+  //runTest(4);
+  //runTest(5);
 //QueenBoard test = new QueenBoard(5);
 //System.out.println(test.countSolutions());
 //System.out.println(test.solve());
@@ -103,5 +126,5 @@ public static int helper2(QueenBoard test, int [][] board, int col){
 //test.removeQueen(0,0);
 
 //System.out.println(test);
-//}
+}
 }
